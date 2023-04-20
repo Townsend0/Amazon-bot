8f98e6c5d253910c865a93c7a2e48dd24c3d8e7a
Townsend0 <126112061+Townsend0@users.noreply.github.com>
2023-04-21 00:11:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import bs4
 import requests
 import smtplib
+import os
 
 url = "https://www.amazon.sa/-/en/HP-Laptop-15s-eq3002nx-RyzenTM-Windows/dp/B0BGXGL91F/ref=sr_1_1?keywords=15s-eq3002nx&qid=1681768792&s=electronics&sr=1-1&th=1"
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3' }
@@ -14,5 +15,5 @@ new_price = float(soup[0] + soup [1].split(".")[0])
 if new_price != old_price:
     a = smtplib.SMTP("smtp.gmail.com", 587)
     a.starttls()
-    a.login("obadahpy@gmail.com", "sohgdzfdmxagagkv")
+    a.login("obadahpy@gmail.com", os.environ["APP_PASSWORD"])
     a.sendmail("obadahpy@gmail.com", "obadah2109@gmail.com", f"Subject:Amazon\n\nnew price = {new_price} SAR")
